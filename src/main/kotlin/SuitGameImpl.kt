7d04b541318a3255b94799da46dd64d0837c074e
Rama Agus <ramap664@gmail.com>
2022-06-30 01:25:19 +0700

    add properties titleGame to primary constructor

--- src/main/kotlin/SuitGameImpl.kt
+++ src/main/kotlin/SuitGameImpl.kt
@@ -1,11 +1,11 @@
-class SuitGameImpl : Game(), SuitGame {
-    private var player1 = ""
+class SuitGameImpl(private val titleGame: String) : Game(titleGame), SuitGame {
+    private var player1 = "" //access modifier
     private var player2 = ""
 
     override fun showMessageTurnPlayer(isPlayer1Turn: Boolean) {
         val mapPlayer = mapOf(true to 1, false to 2)
         val numberPlayer = mapPlayer[isPlayer1Turn]
-        print("$numberPlayer. Masukan pemain $numberPlayer (gunting,batu,kertas): ")
+        print("$numberPlayer. Masukan pemain $numberPlayer (gunting,kertas,batu): ")
     }
 
     override fun readPlayerInput(isPlayer1Turn: Boolean) {
@@ -14,7 +14,7 @@ class SuitGameImpl : Game(), SuitGame {
         if (enteredString.isEmpty()) return //penerapan kondisi
 
         if (enteredString.isNotEmpty()) {
-            println(enteredString)
+//            println(enteredString)
 
             //handle user input
             when (enteredString.lowercase()) {
