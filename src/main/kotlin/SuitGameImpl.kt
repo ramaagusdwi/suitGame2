1785c76ee61cfe8413065a56593e2d973ccba54b
Rama Agus <ramap664@gmail.com>
2022-06-30 00:06:52 +0700

    menambahkan kelas SuitGameImpl

---
+++ src/main/kotlin/SuitGameImpl.kt
@@ -0,0 +1,17 @@
+class SuitGameImpl : Game(), SuitGame {
+
+
+    override fun showMessageTurnPlayer(isPlayer1Turn: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun readPlayerInput() {
+        TODO("Not yet implemented")
+    }
+
+    override fun calculate() {
+        TODO("Not yet implemented")
+    }
+
+
+}
