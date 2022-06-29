2b0620686d0d0e2ccb698425c9930f79f55b0a16
Rama Agus <ramap664@gmail.com>
2022-06-30 02:46:56 +0700

    add game engine

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,5 +1,15 @@
 open class Game(val title: String) {
 
+    private val rules =
+        mapOf(
+            "GUNTING-KERTAS" to true,
+            "GUNTING-BATU" to false,
+            "BATU-GUNTING" to true,
+            "BATU-KERTAS" to false,
+            "KERTAS-BATU" to true,
+            "KERTAS-GUNTING" to false
+        )
+
     fun showWelcomeMessage() {
         println("==========================")
         println(this.title)
@@ -18,5 +28,8 @@ open class Game(val title: String) {
 
     }
 
+    fun isDraw(from: String, to: String): Boolean = from == to
+
+    fun isWin(from: String, to: String): Boolean = rules["$from-$to"]!!
 
 }
