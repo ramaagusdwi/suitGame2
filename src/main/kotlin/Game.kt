264710e5102ef3d33790b29161c461e225e2b0ce
Rama Agus <ramap664@gmail.com>
2022-06-30 00:06:01 +0700

    menambahkan kelas Game dan beberapa fungsi menambahkan tulisan welcome

---
+++ src/main/kotlin/Game.kt
@@ -0,0 +1,21 @@
+open class Game {
+    fun showWelcomeMessage() {
+        println("==========================")
+        println("GAME SUIT TERMINAL VERSION")
+        println("==========================")
+    }
+
+    fun showResultMessage(vararg resultSuit: Int) {
+
+    }
+
+    fun playAgain(isTrue: Boolean) {
+
+    }
+
+    fun showScore(win: Byte, lose: Byte, draw: Byte) {
+
+    }
+
+
+}
