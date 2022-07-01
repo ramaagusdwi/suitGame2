389251dc69e27e83b023599a2411a9faa4acc92d
Rama Agus <ramap664@gmail.com>
2022-07-01 20:36:46 +0700

    handle inputan kosong

--- src/main/kotlin/SuitGameImpl.kt
+++ src/main/kotlin/SuitGameImpl.kt
@@ -2,12 +2,26 @@ class SuitGameImpl(private val titleGame: String) : Game(titleGame), SuitGame {
     internal var player1 = "" //access modifier
     internal var player2 = ""
     private var isPlayer1Turn = false
-
+    private var isCpu = false
+    val optionSuitGame = arrayOf("GUNTING", " KERTAS ", " BATU")
 
     fun startGame(isPlayer1Turn: Boolean) {
         this.isPlayer1Turn = isPlayer1Turn
         showMessageInputan()
         readPlayerInput()
+
+    }
+
+    override fun showMessageVersus() {
+        print("Apakah anda ingin lawan CPU? (y/n): ")
+
+        val enteredString = readLine()!! //take the input from user
+
+        if (enteredString.isEmpty()) return //penerapan kondisi
+
+        if (enteredString.isNotEmpty()) {
+
+        }
     }
 
     override fun showMessageInputan() {
@@ -19,22 +33,25 @@ class SuitGameImpl(private val titleGame: String) : Game(titleGame), SuitGame {
     override fun readPlayerInput() {
         val enteredString = readLine()!! //take the input from user
 
-        if (enteredString.isEmpty()) return //penerapan kondisi
+        if (enteredString.isEmpty()) {
+            println("Mohon inputkan sesuatu!")
+            showMessageInputan()
+            readPlayerInput()
+        }
 
         if (enteredString.isNotEmpty()) {
-            val inputUser = enteredString.trim().uppercase()
-            when (inputUser) {
+            val inputFromUser = enteredString.trim().uppercase()
+            when (inputFromUser) {
+
                 "GUNTING", "KERTAS", "BATU" -> {
-                    if (isPlayer1Turn) player1 = inputUser
-                    else player2 = inputUser
+                    if (isPlayer1Turn) player1 = inputFromUser
+                    else player2 = inputFromUser
                 }
                 else -> {
                     showMessageInputan() //jika inputan bukan dari GUNTING,KERTAS, BATU maka tampilkan pesan inputan
                     readPlayerInput()
                 }
             }
-
-
         }
     }
 
